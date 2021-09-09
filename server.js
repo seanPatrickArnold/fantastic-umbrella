5f81e9d2f42bdc4126805e10fe550165dae1c56d
seanparnold <sean.p.arnold@gmail.com>
2021-09-09 14:44:46 -0700

    added .env'

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 // import sequelize connection
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+const hbs = exphbs.create();
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
